d33a123f87d1d99da3af3ea9f1271f17ebaef2c8
Risto Uibo <risto@rsx.ee>
2024-08-04 11:54:39 +0300

    TransactionStorage: test for current date as part of each stored transaction (green)

--- src/main/kotlin/ee/rsx/kata/codurance/bank/account/Transaction.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/bank/account/Transaction.kt
@@ -2,4 +2,4 @@ package ee.rsx.kata.codurance.bank.account
 
 import java.time.LocalDate
 
-data class Transaction(val amount: Int, val date: LocalDate? = null) {}
+data class Transaction(val amount: Int, val date: LocalDate? = LocalDate.now()) {}
