4140c1f57601f67043553094433d749f727a9c1e
Risto Uibo <risto@rsx.ee>
2024-08-04 22:57:31 +0300

    StatementPrinter: refactor

--- src/main/kotlin/ee/rsx/kata/codurance/bank/account/StatementPrinter.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/bank/account/StatementPrinter.kt
@@ -2,12 +2,14 @@ package ee.rsx.kata.codurance.bank.account
 
 import ee.rsx.kata.codurance.bank.Console
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter.ofPattern
+
 
 class StatementPrinter(private val console: Console) {
 
   fun print(transactions: List<Transaction>) {
-    console.printLine("DATE        | AMOUNT   | BALANCE  |")
+    console.printLine(STATEMENT_HEADER)
 
     var runningTotal = 0
     transactions.map {
@@ -16,17 +18,21 @@ class StatementPrinter(private val console: Console) {
     }
       .reversed()
       .forEach { (transaction, runningTotal) ->
-        console.printLine(transaction.asLine(runningTotal))
+        console.printLine(transaction.asLineWith(runningTotal))
       }
   }
 
-  private fun Transaction.asLine(runningTotal: Int) =
-      "${date.format(ofPattern(DATE_FORMAT))}  | ${amount.format()}   | ${runningTotal.format()}   |"
+  private fun Transaction.asLineWith(runningTotal: Int) =
+      "${date.format()}  | ${amount.format()}   | ${runningTotal.format()}   |"
 
+
+  private fun LocalDate.format() = this.format(ofPattern(DATE_FORMAT))
 
   private fun Int.format() = BigDecimal(this).setScale(2)
 
   companion object {
+    private const val STATEMENT_HEADER = "DATE        | AMOUNT   | BALANCE  |"
+
     const val DATE_FORMAT = "dd/MM/yyyy"
   }
 }
